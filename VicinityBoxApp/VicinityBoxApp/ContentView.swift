339f7e8e0087e129e435df8d96d31558d5b254d3
Nils B <>
2019-12-07 00:25:21 +0100

    iOS first commit

---
+++ VicinityBoxApp/VicinityBoxApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VicinityBoxApp
+//
+//  Created by Nils Brand on 07.12.19.
+//  Copyright © 2019 hypeTech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
