7934767b1917675f9e68d2e6ca728e3eccafd1fd
Nils B <>
2019-12-07 01:04:23 +0100

    iOS small changes

---
+++ VicinityBoxApp/VicinityBoxApp/Views/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  VicinityBoxApp
+//
+//  Created by Nils Brand on 07.12.19.
+//  Copyright © 2019 hypeTech. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
